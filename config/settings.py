9a76e0be0e8aafed7d8023b7af9919c01328124e
jihye0420 <20181054@sungshin.ac.kr>
2023-11-01 17:12:19 +0900

    [CREATE] 회원가입 폼 제외, 로그인 기능 완료, 부트스트랩 적용 완료

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'rest_framework',
     'content',
     'user',
+    'mysite',
+    'pages',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -115,7 +118,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
